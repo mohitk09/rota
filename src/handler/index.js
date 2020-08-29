98040ba849fa4f337e57e3da2d2cbfa24e6fc0d6
Mohit Khotani <mohit.khotani@gmail.com>
2020-08-29 15:54:20 +0530

    get value from secrets manager

--- src/handler/index.js
+++ src/handler/index.js
@@ -1,40 +1,42 @@
 const AWS = require('aws-sdk');
 const dynamodb = new AWS.DynamoDB.DocumentClient();
+const secretsmanager = new AWS.SecretsManager();
 const axios = require('axios');
 
-const url = 'https://api.bamboohr.com/api/gateway.php/peak/v1/time_off/whos_out/';
-
 async function getEmployeeDetails(members) {
-    try {
-      const response = await axios({
-        url,
-        method: 'GET',
-        headers : {
-            'accept': "application/json",
-            'authorization': process.env.API_KEY
-        },
-      });
-      const absentiesList = response.data.filter((item) => {
-           return members.includes(item.name);
-      });
+  
+  const  { SecretString } = await secretsmanager.getSecretValue({ SecretId: 'Bamboo' }).promise();
+  const { API_KEY } = JSON.parse(SecretString);
+  try {
+    const response = await axios({
+      url : 'https://api.bamboohr.com/api/gateway.php/peak/v1/time_off/whos_out/',
+      method: 'GET',
+      headers : {
+          'accept': "application/json",
+          'authorization': API_KEY
+      },
+    });
+    const absentiesList = response.data.filter((item) => {
+          return members.includes(item.name);
+    });
 
-      const today = new Date();
-      const todayDate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-      const absentToday = absentiesList.filter((item) =>{
-          const start = new Date(item.start);
-          const startDate = start.getFullYear()+'-'+(start.getMonth()+1)+'-'+start.getDate();
-          const end = new Date(item.end);
-          const endDate = end.getFullYear()+'-'+(end.getMonth()+1)+'-'+end.getDate();
-          if(todayDate >= startDate  && todayDate <= endDate){
-              return item;
-          }
-      });
-      return absentToday;
-    } catch (error) {
-      console.error(error);
-      return null;
-    }
+    const today = new Date();
+    const todayDate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    const absentToday = absentiesList.filter((item) =>{
+        const start = new Date(item.start);
+        const startDate = start.getFullYear()+'-'+(start.getMonth()+1)+'-'+start.getDate();
+        const end = new Date(item.end);
+        const endDate = end.getFullYear()+'-'+(end.getMonth()+1)+'-'+end.getDate();
+        if(todayDate >= startDate  && todayDate <= endDate){
+            return item;
+        }
+    });
+    return absentToday;
+  } catch (error) {
+    console.error(error);
+    return null;
   }
+}
 
 const rota = async(event) => {
     console.log('event', event);
