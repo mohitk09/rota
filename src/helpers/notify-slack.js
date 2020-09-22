17f1213f7834ef5087a34a8e44460b0495cdbdfb
Mohit Khotani <mohit.khotani@gmail.com>
2020-09-22 14:37:58 +0530

    add set timeout after sending notification

--- src/helpers/notify-slack.js
+++ src/helpers/notify-slack.js
@@ -1,6 +1,11 @@
 var slack = require('slack-notify')(process.env.WEBHOOK_URL);
+
+const wait = (time) => new Promise((resolve) => {
+  setTimeout(() => resolve(), time);
+});
+
 const sendSlackNotifications = async (name, credits) => {
-    slack.send({
+    await slack.send({
         channel: process.env.CHANNEL_NAME ,
         text: `Engineer for today is ${name}`,
         icon_emoji: ':responsible:',
@@ -10,6 +15,8 @@ const sendSlackNotifications = async (name, credits) => {
           },
         color: 'good',
       });
+      await wait(1500);
+      return null;
 };
 
 module.exports = { sendSlackNotifications };
