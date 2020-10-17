4ff3c322df5b6f1f5479898d5adb493eac3bdeac
Mohit Khotani <mohit.khotani@gmail.com>
2020-10-17 23:37:15 +0530

    unavailable for the week function

--- src/handler/pick-member.js
+++ src/handler/pick-member.js
@@ -1,134 +1,116 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-const { sendSlackNotifications } = require('../helpers/notify-slack');
-const { getEmployeeDetails } = require('../helpers/get-employee-details');
+const { sendSlackNotifications } = require("../helpers/notify-slack");
+const { getEmployeeDetails } = require("../helpers/get-employee-details");
 
-const pickMember = async(event) => {
-    const { stage } = process.env;
-    const { teamName, source, name } = event;
-    let peopleUnavailable = [];
-    try{
-      const params = {
-        TableName: `ais-${stage}-rota`,
-        KeyConditionExpression: '#teamName = :teamName',
-        ExpressionAttributeNames: {
-          '#teamName': 'teamName',
-        },
-        ExpressionAttributeValues: {
-          ':teamName': teamName,
-        },
-      };
+const pickMember = async event => {
+  const { stage } = process.env;
+  const { teamName, source, name } = event;
+  let peopleUnavailable = [];
+  try {
+    const params = {
+      TableName: `ais-${stage}-rota`,
+      KeyConditionExpression: "#teamName = :teamName",
+      ExpressionAttributeNames: {
+        "#teamName": "teamName"
+      },
+      ExpressionAttributeValues: {
+        ":teamName": teamName
+      }
+    };
 
     const { Items } = await dynamodb.query(params).promise();
     const { members, daysElapsed } = Items[0];
 
-    if(!source){
-      members.forEach((item) =>{
-        if(item.name === name){
+    if (!source) {
+      members.forEach(item => {
+        if (item.name === name) {
           item.credits -= 1;
           item.current = item.previous;
         }
       });
 
-      const updateParams =  {
+      const updateParams = {
         ExpressionAttributeNames: {
-        "#members": "members", 
-        "#peopleUnavailable": "peopleUnavailable",
-        }, 
-        ExpressionAttributeValues: {
-        ':peopleUnavailable': [name],
-        ':empty_list': [],
-        ':members': members
-        }, 
-        Key: {
-          'teamName': teamName
+          "#members": "members"
         },
-        ReturnValues: 'ALL_NEW', 
-        TableName: `ais-${stage}-rota`, 
-        UpdateExpression: "SET #peopleUnavailable = list_append(if_not_exists(#peopleUnavailable, :empty_list), :peopleUnavailable), #members = :members"
-      };
-      const response = await dynamodb.update(updateParams).promise();
-      peopleUnavailable = response.Attributes.peopleUnavailable; 
-      console.log('resp1 ---- ', response);
-      console.log('people un--', peopleUnavailable); 
-
-    }else{
-      const updateParams =  {
-        ExpressionAttributeNames: {
-        "#peopleUnavailable": "peopleUnavailable",
-        }, 
         ExpressionAttributeValues: {
-        ':peopleUnavailable': [],
-        }, 
-        Key: {
-          'teamName': teamName
+          ":members": members
         },
-        ReturnValues: 'ALL_NEW', 
-        TableName: `ais-${stage}-rota`, 
-        UpdateExpression: "SET #peopleUnavailable = :peopleUnavailable"
+        Key: {
+          teamName: teamName
+        },
+        ReturnValues: "ALL_NEW",
+        TableName: `ais-${stage}-rota`,
+        UpdateExpression: "SET #members = :members"
       };
       await dynamodb.update(updateParams).promise();
-
     }
 
     // sorting the members so that minimum credit person is picked first
     members.sort((a, b) => {
-        if(a.credits === b.credits) return a.current-b.current;
-        return a.credits-b.credits;
+      // in case the credits are equal person who did last would be picked first
+      if (a.credits === b.credits) return a.current - b.current;
+      return a.credits - b.credits;
     });
-    const memberNames = members.map((item) =>  item.name);
+    const memberNames = members.map(item => item.name);
 
     let absentiesList = [];
-    if(source)
-      absentiesList  = await getEmployeeDetails(memberNames);
+    if (source) absentiesList = await getEmployeeDetails(memberNames);
 
-    console.log('memmbes', members);
-    
+    console.log("memmbes", members);
+
     //check availability of member here and also decrement the count of the selected member
     let personSelected = members[0].name;
     let personCredits = members[0].credits;
-    for(let i=0;i<members.length;i++){
-      if(!absentiesList.includes(members[i].name) && !peopleUnavailable.includes(members[i].name)){
+    for (let i = 0; i < members.length; i++) {
+      if (
+        !absentiesList.includes(members[i].name) &&
+        !peopleUnavailable.includes(members[i].name)
+      ) {
         personSelected = members[i].name;
         personCredits = members[i].credits;
         break;
       }
     }
-    
-    members.forEach((item) =>{
-      if(item.name === personSelected){
+
+    members.forEach(item => {
+      if (item.name === personSelected) {
         item.credits += 1;
         item.previous = item.current;
         item.current = new Date().valueOf();
       }
     });
-    const updateParams =  {
+    const updateParams = {
       ExpressionAttributeNames: {
-      "#members": "members", 
-      "#daysElapsed": "daysElapsed",
-      "#peopleUnavailable": "peopleUnavailable"
-      }, 
-      ExpressionAttributeValues: {
-      ':members': members,
-      ':daysElapsed': daysElapsed+1,
-      ':peopleUnavailable': absentiesList
-      }, 
-      Key: {
-        'teamName': teamName
+        "#members": "members",
+        "#daysElapsed": "daysElapsed",
+        "#peopleUnavailable": "peopleUnavailable"
       },
-      ReturnValues: 'ALL_NEW', 
-      TableName: `ais-${stage}-rota`, 
-      UpdateExpression: "SET #members = :members, #daysElapsed = :daysElapsed, #peopleUnavailable = :peopleUnavailable"
+      ExpressionAttributeValues: {
+        ":members": members,
+        ":daysElapsed": daysElapsed + 1,
+        ":peopleUnavailable": absentiesList
+      },
+      Key: {
+        teamName: teamName
+      },
+      ReturnValues: "ALL_NEW",
+      TableName: `ais-${stage}-rota`,
+      UpdateExpression:
+        "SET #members = :members, #daysElapsed = :daysElapsed, #peopleUnavailable = :peopleUnavailable"
     };
 
-    await Promise.all([dynamodb.update(updateParams).promise(),
-      sendSlackNotifications(personSelected, personCredits) ]);
-    console.log('Engineer for today is', personSelected);
+    await Promise.all([
+      dynamodb.update(updateParams).promise(),
+      sendSlackNotifications(personSelected, personCredits)
+    ]);
+    console.log("Engineer for today is", personSelected);
     return null;
-  }catch(error){
-      console.log(error);
-      return null;
+  } catch (error) {
+    console.log(error);
+    return null;
   }
 };
 
--- src/helpers/get-employee-details.js
+++ src/helpers/get-employee-details.js
@@ -1,38 +1,117 @@
-const AWS = require('aws-sdk');
-
+const AWS = require("aws-sdk");
 const secretsmanager = new AWS.SecretsManager();
-const axios = require('axios');
+const axios = require("axios");
 
-async function getEmployeeDetails(members) {
-    try {
-    const  { SecretString } = await secretsmanager.getSecretValue({ SecretId: 'Bamboo' }).promise();
-    const { API_KEY } = JSON.parse(SecretString);
-    const response = await axios({
-        url : 'https://api.bamboohr.com/api/gateway.php/peak/v1/time_off/whos_out/',
-        method: 'GET',
-        headers : {
-            'accept': "application/json",
-            'authorization': API_KEY
-        },
+/*
+The below function calls the bamboo api to find people on leave, if a person is on leave for 3 days
+or above in a week this function would include that person in the list.
+*/
+async function UnavailableForMostOfTheWeek(members) {
+  try {
+    const response = await callBambooApi();
+    const absentiesList = (response.data || []).filter(item =>
+      members.includes(item.name)
+    );
+    absentiesList.forEach(item => {
+      var date1 = new Date(item.start);
+      var date2 = new Date(item.end);
+      var timeDiff = date2.getTime() - date1.getTime();
+      var daysDiff = timeDiff / (1000 * 3600 * 24) + 1;
+      const today = new Date();
+      const todayDate =
+        today.getFullYear() +
+        "-" +
+        (today.getMonth() + 1) +
+        "-" +
+        today.getDate();
+      let endDateOfTheWeek = addDays(todayDate, 4);
+      endDateOfTheWeek =
+        endDateOfTheWeek.getFullYear() +
+        "-" +
+        (endDateOfTheWeek.getMonth() + 1) +
+        "-" +
+        endDateOfTheWeek.getDate();
+
+      console.log(endDateOfTheWeek);
     });
-    const absentiesList = (response.data || []).filter((item) => {
-            return members.includes(item.name);
-    });
-    const today = new Date();
-    const todayDate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    const absentToday = absentiesList.filter((item) =>{
-    const start = new Date(item.start);
-    const startDate = start.getFullYear()+'-'+(start.getMonth()+1)+'-'+start.getDate();
-    const end = new Date(item.end);
-    const endDate = end.getFullYear()+'-'+(end.getMonth()+1)+'-'+end.getDate();
-    if(todayDate >= startDate  && todayDate <= endDate) return item;
-    }).map(({ name }) =>  name);
-    return absentToday;
-    } catch (error) {
+  } catch (error) {
     console.error(error);
     throw error;
-    }
+  }
 }
 
-module.exports = { getEmployeeDetails };
+async function callBambooApi() {
+  try {
+    const { SecretString } = await secretsmanager
+      .getSecretValue({ SecretId: "Bamboo" })
+      .promise();
+    const { API_KEY } = JSON.parse(SecretString);
+    const response = await axios({
+      url:
+        "https://api.bamboohr.com/api/gateway.php/peak/v1/time_off/whos_out/",
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        authorization: API_KEY
+      }
+    });
+    return response;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
 
+async function absentToday(members) {
+  try {
+    const response = await callBambooApi();
+    const absentiesList = (response.data || []).filter(item =>
+      members.includes(item.name)
+    );
+    const today = new Date();
+    const todayDate =
+      today.getFullYear() +
+      "-" +
+      (today.getMonth() + 1) +
+      "-" +
+      today.getDate();
+    const absentToday = absentiesList
+      .filter(item => {
+        const start = new Date(item.start);
+        const startDate =
+          start.getFullYear() +
+          "-" +
+          (start.getMonth() + 1) +
+          "-" +
+          start.getDate();
+        const end = new Date(item.end);
+        const endDate =
+          end.getFullYear() + "-" + (end.getMonth() + 1) + "-" + end.getDate();
+        if (todayDate >= startDate && todayDate <= endDate) return item;
+      })
+      .map(({ name }) => name);
+    return absentToday;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+const members = [
+  "Lauren Rodgers",
+  "Michael Harrison",
+  "Syed",
+  "Michael Pearce",
+  "Adam Waterhouse",
+  "Deepak Rathee",
+  "Chris Newton",
+  "Mohit Khotani"
+];
+
+function addDays(date, days) {
+  var result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
+}
+
+UnavailableForMostOfTheWeek(members);
+module.exports = { absentToday, UnavailableForMostOfTheWeek };
