06179f1e99136705ce7706d015c3bfd8a684458a
Mohit Khotani <mohit.khotani@gmail.com>
2020-11-12 15:05:36 +0530

    promisify slack send

--- src/helpers/notify-slack.js
+++ src/helpers/notify-slack.js
@@ -1,22 +1,22 @@
 var slack = require("slack-notify")(process.env.WEBHOOK_URL);
 
-const wait = time =>
-  new Promise(resolve => {
-    setTimeout(() => resolve(), time);
-  });
 
 const sendSlackNotifications = async (name, credits, message) => {
-  await slack.send({
-    channel: process.env.CHANNEL_NAME,
-    text: `${message} ${name}`,
-    icon_emoji: ":responsible:",
-    username: "ROTA",
-    fields: {
-      "Number of days done": credits
-    },
-    color: "good"
+  await new Promise((resolve, reject) => {
+    slack.send({
+      channel: process.env.CHANNEL_NAME,
+      text: `${message} ${name}`,
+      icon_emoji: ":responsible:",
+      username: "ROTA",
+      fields: {
+        "Number of days done": credits
+      },
+      color: "good"
+    }, (err) => {
+      if (err) reject(err);
+      else resolve(null);
+    });
   });
-  await wait(1500);
   return null;
 };
 
